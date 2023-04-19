e4da66056db73765e42e7fcc29949a4b5969b08e
Igor <tararuev65@gmail.com>
2023-04-19 20:01:47 +0600

    ExC005_MASHA

---
+++ ExC005_ConditionIF/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Введите имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша") //ToLower() - убирает все варианты маша (Маша=мАша =МАша итд)
+{
+    Console.WriteLine("Yes! это МАША!");
+}
+else
+{
+Console.Write("Привет, ");
+Console.WriteLine(username );
+}
