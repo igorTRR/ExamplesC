ec142aaebbf9f14baae869d310b2f29a450f1adf
Igor <tararuev65@gmail.com>
2023-04-20 12:14:49 +0600

    Generator

---
+++ ExC011_FullArray/Program.cs
@@ -0,0 +1,21 @@
+﻿ // Генератор случ.чисел мин/макс
+
+Console.Clear();
+
+int[] GetArray (int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for(int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);   
+    }
+    return result;
+}
+int[] array = GetArray(10, -5, 18);
+Console.WriteLine(String.Join(", ", array));
+
+
+    
+
+
